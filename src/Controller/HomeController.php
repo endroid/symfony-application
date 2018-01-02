6d3d0d62aab1fd52ca3173e449d5d138916bc662
endroid <info@endroid.nl>
2018-01-03 00:33:35 +0100

    Remove configuration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,9 @@
 
 namespace App\Controller;
 
+use Endroid\Sudoku\Board\Board;
+use Endroid\Sudoku\Board\BoardBuilder;
+use Endroid\Sudoku\Solver;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -20,6 +23,41 @@ final class HomeController
      */
     public function __invoke(Environment $twig): Response
     {
+//        $platinumBlonde = Board::createFromString('
+//            000000012
+//            000000003
+//            002300400
+//            001800005
+//            060070800
+//            000009000
+//            008500000
+//            900040500
+//            470006000
+//        ');
+//
+//        $solveWithoutGuessing = Board::createFromString('
+//            003020600
+//            900305001
+//            001806400
+//            008102900
+//            700000008
+//            006708200
+//            002609500
+//            800203009
+//            005010300
+//        ');
+//
+//        $sudoku = $platinumBlonde;
+//        $solver = new Solver($sudoku);
+//        $solver->solve(true);
+//
+//        echo $sudoku->toHtmlString();
+//        die;
+//
+//        $boardBuilder = new BoardBuilder();
+
+
+
         return new Response($twig->render('home.html.twig'));
     }
 }
