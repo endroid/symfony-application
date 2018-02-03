20d7614c9a95f0e5e865405c0a47123eddce166c
endroid <info@endroid.nl>
2018-02-03 13:33:14 +0000

    Refactor frontend

--- frontend/js/base.js
+++ frontend/js/base.js
@@ -1,6 +1,10 @@
-global.Popper = require('popper.js/dist/umd/popper'); // Used in Bootstrap 4 dropdowns, tooltips and popovers
+// Popper is used by Bootstrap 4
+global.Popper = require('popper.js/dist/umd/popper');
 
 require('bootstrap/dist/css/bootstrap.css');
 require('bootstrap/dist/js/bootstrap.js');
 require('font-awesome/css/font-awesome.css');
+require('select2/dist/js/select2.js');
+require('select2/dist/css/select2.css');
 require('./../css/base.css');
+require('./../js/search.js');
