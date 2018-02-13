82be286500160f17fbe900d9a31568c8022d725d
endroid <info@endroid.nl>
2018-02-13 22:18:31 +0000

    Temporarily disable search

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
-    $('#search').select2({
-        placeholder: 'Search',
-        ajax: {
-            url: '/search/suggest',
-            dataType: 'json',
-            delay: 250,
-            data: function (params) {
-                return {
-                    query: params.term
-                }
-            },
-            processResults: function (data, params) {
-                return {
-                    results: data
-                }
-            }
-        },
-        cache: true,
-        minimumInputLength: 1
-    });
-});
+// $(document).ready(function() {
+//     $('#search').select2({
+//         placeholder: 'Search',
+//         ajax: {
+//             url: '/search/suggest',
+//             dataType: 'json',
+//             delay: 250,
+//             data: function (params) {
+//                 return {
+//                     query: params.term
+//                 }
+//             },
+//             processResults: function (data, params) {
+//                 return {
+//                     results: data
+//                 }
+//             }
+//         },
+//         cache: true,
+//         minimumInputLength: 1
+//     });
+// });
