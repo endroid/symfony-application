2942ba764e8eacac335e3f45755eb6e34cedcd5f
endroid <info@endroid.nl>
2018-04-08 22:18:40 +0000

    Refactor PDF and use installer

--- src/Controller/Pdf/PdfController.php
+++ src/Controller/Pdf/PdfController.php
@@ -9,21 +9,18 @@
 
 namespace App\Controller\Pdf;
 
-use Endroid\Pdf\Factory\AssetFactory;
-use Endroid\Pdf\Pdf;
+use Endroid\Pdf\Builder\PdfBuilder;
 use Endroid\Pdf\Response\InlinePdfResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class PdfController
 {
-    private $assetFactory;
-    private $pdf;
+    private $pdfBuilder;
 
-    public function __construct(AssetFactory $assetFactory, Pdf $pdf)
+    public function __construct(PdfBuilder $pdfBuilder)
     {
-        $this->assetFactory = $assetFactory;
-        $this->pdf = $pdf;
+        $this->pdfBuilder = $pdfBuilder;
     }
 
     /**
@@ -31,33 +28,20 @@ final class PdfController
      */
     public function __invoke(): Response
     {
-        $this->pdf->setCover($this->assetFactory->create([
-            'controller' => CoverController::class,
-            'cache' => 'cover',
-        ]));
-        $this->pdf->setTableOfContents($this->assetFactory->create([
-            'template' => 'pdf/table_of_contents.xml.twig',
-            'cache' => 'toc',
-        ]));
-        $this->pdf->setHeader($this->assetFactory->create([
-            'template' => 'pdf/header.html.twig',
-            'cache' => 'header',
-        ]));
-        $this->pdf->setFooter($this->assetFactory->create([
-            'template' => 'pdf/footer.html.twig',
-            'cache' => 'footer',
-        ]));
-        $this->pdf->setContent($this->assetFactory->create([
-            'controller' => ContentController::class,
-            'cache' => 'content',
-        ]));
-        $this->pdf->setOptions([
-            'margin-top' => 16,
-            'margin-bottom' => 16,
-            'header-spacing' => 5,
-            'footer-spacing' => 5,
-        ]);
+        $this->pdfBuilder
+            ->setCover(['controller' => CoverController::class, 'cache' => 'cover'])
+            ->setTableOfContents(['template' => 'pdf/table_of_contents.xml.twig', 'cache' => 'toc'])
+            ->setHeader(['template' => 'pdf/header.html.twig', 'cache' => 'header'])
+            ->setFooter(['template' => 'pdf/footer.html.twig', 'cache' => 'footer'])
+            ->setContent(['controller' => ContentController::class, 'cache' => 'content'])
+            ->setOptions([
+                'margin-top' => 16,
+                'margin-bottom' => 16,
+                'header-spacing' => 5,
+                'footer-spacing' => 5,
+            ])
+        ;
 
-        return new InlinePdfResponse($this->pdf);
+        return new InlinePdfResponse($this->pdfBuilder->getPdf());
     }
 }
