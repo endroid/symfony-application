bce901febf8f473b5505c66b85c2e66f4e30c9c0
endroid <info@endroid.nl>
2018-04-10 16:59:19 +0200

    Truncate without FK checks in test

---
+++ src/DBAL/Platform.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\DBAL;
+
+use Doctrine\DBAL\Platforms\MySqlPlatform;
+
+class Platform extends MySqlPlatform
+{
+    public function getTruncateTableSQL($tableName, $cascade = false)
+    {
+        return sprintf('SET foreign_key_checks = 0;TRUNCATE %s;SET foreign_key_checks = 1;', $tableName);
+    }
+}
