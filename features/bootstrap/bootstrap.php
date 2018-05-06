161630fc6a7f4af9dd3d3f9eeb9eef106347e209
endroid <info@endroid.nl>
2018-05-06 21:44:47 +0000

    Simplify bootstrap

--- features/bootstrap/bootstrap.php
+++ features/bootstrap/bootstrap.php
@@ -2,15 +2,10 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-// The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
-    }
-    (new Dotenv())->load(__DIR__.'/../../application/.env');
-}
+$dotEnv = new DotEnv();
+$dotEnv->load(__DIR__.'/../../application/.env');
+$dotEnv->populate(['APP_ENV' => 'test']);
 
-passthru('php "'.__DIR__.'/../../application/bin/console" cache:clear --no-warmup -q');
-passthru('php "'.__DIR__.'/../../application/bin/console" doctrine:database:create --if-not-exists -n -q');
-passthru('php "'.__DIR__.'/../../application/bin/console" doctrine:schema:update --force -n -q');
-passthru('php "'.__DIR__.'/../../application/bin/console" fos:elastica:populate  -n -q');
+passthru('bin/console cache:clear --env=test -q');
+passthru('bin/console doctrine:database:create --if-not-exists --env=test -n -q');
+passthru('bin/console doctrine:schema:update --force --env=test -n -q');
