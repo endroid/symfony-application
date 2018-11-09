433e983033603cbbd26426e9881386542daff712
endroid <info@endroid.nl>
2018-11-09 15:20:19 +0000

    Speed up tests

--- development/features/bootstrap/FeatureContext.php
+++ development/features/bootstrap/FeatureContext.php
@@ -7,11 +7,30 @@ use Symfony\Component\Process\Process;
 class FeatureContext extends MinkContext
 {
     /**
-     * @BeforeScenario
+     * @BeforeSuite
+     * @AfterScenario @reset
      */
-    public function loadFixtures()
+    public static function reset()
+    {
+        self::resetFixtures();
+        self::resetElasticsearch();
+    }
+
+    private static function resetFixtures()
     {
         $command = 'bin/console doctrine:fixtures:load --env=test --purge-with-truncate -n -q';
+
+        $process = new Process($command);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+    }
+
+    private static function resetElasticsearch()
+    {
+        $command = 'bin/console fos:elastica:populate --env=test -n -q';
 
         $process = new Process($command);
         $process->run();
