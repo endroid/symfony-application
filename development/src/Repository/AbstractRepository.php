298240876d3387cef75bd320411aec05d7887219
Jeroen van den Enden <jvde@netvlies.nl>
2018-11-23 09:21:21 +0000

    Fix CS

--- development/src/Repository/AbstractRepository.php
+++ development/src/Repository/AbstractRepository.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Repository;
 
-use App\Entity\Example;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
--- development/src/Repository/UserRepository.php
+++ development/src/Repository/UserRepository.php
@@ -1,10 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Repository;
 
 use App\Entity\User;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class UserRepository extends AbstractRepository
 {
