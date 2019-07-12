27b5615756ce4c76800da1185908a4f14e6d75d6
Jeroen van den Enden <info@endroid.nl>
2019-07-12 22:22:35 +0000

    Update

--- development/src/Admin/UserAdmin.php
+++ development/src/Admin/UserAdmin.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Admin;
 
+use FOS\UserBundle\Model\UserManagerInterface;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
@@ -13,14 +14,34 @@ class UserAdmin extends AbstractAdmin
 {
     protected $datagridValues = [
         '_sort_order' => 'ASC',
-        '_sort_by' => 'email',
+        '_sort_by' => 'username',
     ];
 
     protected $translationDomain = 'admin';
 
+    private $userManager;
+
+    public function __construct(string $code, string $class, string $baseControllerName, UserManagerInterface $userManager)
+    {
+        parent::__construct($code, $class, $baseControllerName);
+
+        $this->userManager = $userManager;
+    }
+
     public function configure(): void
     {
         unset($this->listModes['mosaic']);
+    }
+
+    public function prePersist($user): void
+    {
+        $this->preUpdate($user);
+    }
+
+    public function preUpdate($user): void
+    {
+        $this->userManager->updateCanonicalFields($user);
+        $this->userManager->updatePassword($user);
     }
 
     protected function configureFormFields(FormMapper $formMapper): void
@@ -28,7 +49,12 @@ class UserAdmin extends AbstractAdmin
         $formMapper
             ->tab('admin.general.label')
                 ->with('admin.user.administration', ['class' => 'col-md-6'])
+                    ->add('username', null, ['label' => 'admin.user.username'])
                     ->add('email', null, ['label' => 'admin.user.email'])
+                    ->add('enabled', null, ['label' => 'admin.user.enabled'])
+                ->end()
+                ->with('admin.group.label', ['class' => 'col-md-6'])
+                    ->add('groups', null, ['label' => 'admin.group.label'])
                 ->end()
             ->end()
         ;
@@ -37,6 +63,7 @@ class UserAdmin extends AbstractAdmin
     protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
     {
         $datagridMapper
+            ->add('username', null, ['label' => 'admin.user.username'])
             ->add('email', null, ['label' => 'admin.user.email'])
         ;
     }
@@ -44,7 +71,9 @@ class UserAdmin extends AbstractAdmin
     protected function configureListFields(ListMapper $listMapper): void
     {
         $listMapper
-            ->addIdentifier('email', null, ['label' => 'admin.user.email'])
+            ->addIdentifier('username', null, ['label' => 'admin.user.username'])
+            ->add('enabled', null, ['label' => 'admin.user.enabled'])
+            ->add('groups', null, ['label' => 'admin.group.label'])
         ;
     }
 }
--- development/src/Entity/User.php
+++ development/src/Entity/User.php
@@ -5,117 +5,27 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ApiResource()
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Entity
+ * @ORM\Table(name="user")
  */
-class User implements UserInterface
+class User extends BaseUser implements UserInterface
 {
     /**
-     * @ORM\Id()
-     * @ORM\Column(type="string")
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
     /**
-     * @ORM\Column(type="string", length=180, unique=true)
+     * @ORM\ManyToMany(targetEntity="App\Entity\Group", inversedBy="users")
+     * @ORM\JoinTable(name="user_user_group")
      */
-    private $username;
-
-    /**
-     * @ORM\Column(type="string", length=180, unique=true)
-     */
-    private $email;
-
-    /**
-     * @ORM\Column(type="string")
-     */
-    private $password;
-    private $plainPassword = '';
-
-    /**
-     * @ORM\Column(type="json")
-     */
-    private $roles = [];
-
-    public function __construct(string $id, string $username, string $email)
-    {
-        $this->id = $id;
-        $this->username = $username;
-        $this->email = $email;
-
-        $this->plainPassword = random_bytes(20);
-        $this->roles = ['ROLE_USER'];
-    }
-
-    public function getId(): string
-    {
-        return $this->id;
-    }
-
-    public function getUsername(): string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): void
-    {
-        $this->username = $username;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): void
-    {
-        $this->email = $email;
-    }
-
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): void
-    {
-        $this->password = $password;
-    }
-
-    public function getPlainPassword(): string
-    {
-        return $this->plainPassword;
-    }
-
-    public function setPlainPassword(string $plainPassword): void
-    {
-        $this->plainPassword = $plainPassword;
-    }
-
-    public function getRoles(): array
-    {
-        return $this->roles;
-    }
-
-    public function setRoles(array $roles): void
-    {
-        $roles[] = 'ROLE_USER';
-
-        $this->roles = array_unique($roles);
-    }
-
-    public function getSalt()
-    {
-        // not needed when using the "bcrypt" algorithm
-    }
-
-    public function eraseCredentials()
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
+    protected $groups;
 }
