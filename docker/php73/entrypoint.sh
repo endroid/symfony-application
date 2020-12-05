b07beb44dfd727adcde2631f683aaf63c9bcf062
Jeroen van den Enden <info@endroid.nl>
2020-12-05 10:37:05 +0000

    Update docker stack

--- docker/php73/entrypoint.sh
+++ docker/php73/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # BLACKFIRE
 
-if [ $BLACKFIRE_SERVER_ID != "" ] && [ $BLACKFIRE_SERVER_TOKEN != "" ]; then
+if [ "$BLACKFIRE_SERVER_ID" != "" ] && [ "$BLACKFIRE_SERVER_TOKEN" != "" ]; then
     sed -i 's/^server-id.*$/server-id='$BLACKFIRE_SERVER_ID'/' /etc/blackfire/agent
     sed -i 's/^server-token.*$/server-token='$BLACKFIRE_SERVER_TOKEN'/' /etc/blackfire/agent
     sed -i 's/^blackfire.server_id.*$/blackfire.server_id = '$BLACKFIRE_SERVER_ID'/' /usr/local/etc/php/conf.d/zzzz-config.ini
@@ -12,19 +12,21 @@ fi
 
 # GITHUB OAUTH
 
-if [ $GITHUB_OAUTH_TOKEN != "" ]; then
-    mkdir -p /var/www/.composer
-    chmod -R 777 /var/www/.composer
+if [ "$GITHUB_OAUTH_TOKEN" != "" ]; then
+    mkdir -p /var/www/.composer /root/.composer
+    chmod -R 777 /var/www/.composer /root/.composer
     echo "{ \"github-oauth\": { \"github.com\": \"$GITHUB_OAUTH_TOKEN\" }}" > /var/www/.composer/auth.json
     echo "{ \"github-oauth\": { \"github.com\": \"$GITHUB_OAUTH_TOKEN\" }}" > /root/.composer/auth.json
 fi
 
 # ERROR REPORTING
 
-if [ $ENVIRONMENT != DEV ]; then
+if [ "$ENVIRONMENT" != "DEV" ]; then
     sed -i 's/^display_startup_errors.*$/display_startup_errors = Off/' /usr/local/etc/php/conf.d/zzzz-config.ini
     sed -i 's/^display_errors.*$/display_errors = Off/' /usr/local/etc/php/conf.d/zzzz-config.ini
 else
     sed -i 's/^display_startup_errors.*$/display_startup_errors = On/' /usr/local/etc/php/conf.d/zzzz-config.ini
     sed -i 's/^display_errors.*$/display_errors = On/' /usr/local/etc/php/conf.d/zzzz-config.ini
 fi
+
+composer self-update
