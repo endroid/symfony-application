930c280b9287249f8e4521a281d6aa12d5f90ce2
Jeroen van den Enden <info@endroid.nl>
2021-06-20 15:20:33 +0000

    Remove old blackfire initialization

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# BLACKFIRE
-
-if [ "$BLACKFIRE_SERVER_ID" != "" ] && [ "$BLACKFIRE_SERVER_TOKEN" != "" ]; then
-    sed -i 's/^server-id.*$/server-id='$BLACKFIRE_SERVER_ID'/' /etc/blackfire/agent
-    sed -i 's/^server-token.*$/server-token='$BLACKFIRE_SERVER_TOKEN'/' /etc/blackfire/agent
-    sed -i 's/^blackfire.server_id.*$/blackfire.server_id = '$BLACKFIRE_SERVER_ID'/' /usr/local/etc/php/conf.d/zzzz-config.ini
-    sed -i 's/^blackfire.server_token.*$/blackfire.server_token = '$BLACKFIRE_SERVER_TOKEN'/' /usr/local/etc/php/conf.d/zzzz-config.ini
-    /etc/init.d/blackfire-agent start
-fi
-
 # GITHUB OAUTH
 
 if [ "$GITHUB_OAUTH_TOKEN" != "" ]; then
